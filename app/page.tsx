cd3e87044046de8adaa473fe1fb7baea035a2e0d
Bob Anderson <rca12377@gmail.com>
2024-05-29 12:54:08 -0700

    from VS CODE

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,15 @@ export default function Home() {
 
 
     <p>CAN YOU SEE ME?</p>
-    
+
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-<<<<<<< HEAD
-          
-        <br/><br/><B>TEST TEST TEST</B><br/><br/>
-          
-=======
+
+
           <p>
           TEST TEST TEST <br/>WOO HOO<br/>
           </p>
->>>>>>> c058b21d851667b008bb5679e506e7fecceb17e9
+
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
