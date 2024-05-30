0675583d9a699968b6c4b6de33693b45b353562b
Bob Anderson <rca12377@gmail.com>
2024-05-30 11:24:42 -0700

    5/30

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
        
       <Image
-        src="/coasys.png"
+        src="/coasys.svg"
         width={150}
         height={46}
         alt="Coasys Logo Image"
