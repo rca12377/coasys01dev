1834eae42e94cfd4441b92962b535cf5d9da02fd
Bob Anderson <rca12377@gmail.com>
2024-05-30 11:21:55 -0700

    5/30

--- app/page.tsx
+++ app/page.tsx
@@ -8,20 +8,14 @@ export const dynamic = 'force-dynamic';
 
 export default function Home() {
   return (
+
     <main id="mainDiv" className="flex min-h-screen flex-col items-center justify-between p-24">
 
 
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-
-
-          <p>
-            5/30 <br/>
-            COASYS WEBSITE<br/>TEST TEST<br/>
-          </p>
-
+          A Digital Nervous System for the New Web
         </p>
-       
       </div>
 
 
@@ -35,6 +29,10 @@ export default function Home() {
         />
 
       </div>
+
+
+
+
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
