8298b5f6a5714e3c86a345dfd59dd5b23e6a39ee
Bob Anderson <rca12377@gmail.com>
2024-05-30 11:52:02 -0700

    5/30

--- app/page.tsx
+++ app/page.tsx
@@ -28,20 +28,6 @@ export default function Home() {
         alt="Coasys Logo Image"
         />
 
-
-      <Image
-        src="/fluxLogo_white.svg"
-        height={46}
-        width={150}
-        alt="FLUX Logo Image"
-        />
-
-      <Image
-        src="/synergyLogo_white.svg"
-        height={46}
-        width={150}
-        alt="Synergy Logo Image"
-        />
       </div>
 
 
@@ -55,15 +41,35 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          
           <Image
               src="/ad4mLogo_white.svg"
               height={46}
               width={150}
               alt="AD4M Logo Image"
               />
-          
+            <br/>
+            <p className="m-0 max-w-[30ch] text-sm opacity-50">
+              AD4m blah blah blah ...
+            </p>
+        </a>
+
+        <a
+          href="#"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+
+        <Image
+          src="/fluxLogo_white.svg"
+          height={46}
+          width={150}
+          alt="FLUX Logo Image"
+          />
+          <br/>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            AD4m blah blah blah ...
+            FLUX blah blah blah
           </p>
         </a>
 
@@ -73,51 +79,21 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            FLUX{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
+
+        <Image
+          src="/synergyLogo_white.svg"
+          height={46}
+          width={150}
+          alt="Synergy Logo Image"
+          />
+          <br/>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Synergy blah blah blah
           </p>
         </a>
 
-        <a
-          href="#"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            TEST{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
+
     </main>
   );
 }
