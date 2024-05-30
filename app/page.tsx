b0d45c8df3dbc355a46d1e3502866ded20f664ee
Bob Anderson <rca12377@gmail.com>
2024-05-30 07:49:44 -0700

    5/30

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,6 @@ export default function Home() {
           COASYS WEBSITE<br/>
           </p>
 
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
