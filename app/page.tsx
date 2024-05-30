714dbdb259c5ca2375aa17ab7530a881338f884d
Bob Anderson <rca12377@gmail.com>
2024-05-30 08:23:29 -0700

    5/30

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,33 @@
 import Image from "next/image";
 
-export const revalidate = 0;
+
+/** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
+
+const nextDataIndex = runtimeCaching.findIndex(
+  (entry) => entry.options.cacheName === "next-data"
+);
+
+if (nextDataIndex !== -1) {
+  runtimeCaching[nextDataIndex].handler = "NetworkFirst";
+} else {
+  throw new Error("Failed to find next-data object in runtime caching");
+}
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  runtimeCaching,
+  register: true,
+  skipWaiting: true,
+  buildExcludes: [/middleware-manifest.json$/],
+  disable: process.env.NODE_ENV === 'development'
+})
+
+module.exports = withPWA({
+  reactStrictMode: false
+});
+
+
 
 export default function Home() {
   return (
