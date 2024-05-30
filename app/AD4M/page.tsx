ea4a58b2ce6fc422a799032590710b8e3e81f194
rca12377 <rca12377@gmail.com>
2024-05-30 15:29:40 -0700

    5/30

--- app/AD4M/page.tsx
+++ app/AD4M/page.tsx
@@ -1,7 +1,7 @@
 export default function AD4M() {
     return (
 
-        <main>
+        <main id="mainDiv" className="flex min-h-screen flex-col items-center justify-between p-24">
             <h2>Hello AD4M</h2>
         </main>
     );
