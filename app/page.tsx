4ff9c599cb386b48729fdd536851f0394aee29eb
rca12377 <rca12377@gmail.com>
2024-05-30 16:28:18 -0700

    5/30

--- app/page.tsx
+++ app/page.tsx
@@ -27,9 +27,6 @@ export default function Home() {
       </div>
 
 
-
-
-
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
           href="#"
