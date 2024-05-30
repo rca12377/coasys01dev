a71ed49d959c535fd867e264821c9e15a4079e0f
Bob Anderson <rca12377@gmail.com>
2024-05-30 07:45:44 -0700

    5/30

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{" "}
-            <Image
+            <Image TEST
               src="/vercel.svg"
               alt="Vercel Logo"
               className="dark:invert"
