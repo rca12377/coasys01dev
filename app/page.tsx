5250e39341ebaf6b784a430556f7b1ddf7a3beca
rca12377 <rca12377@gmail.com>
2024-05-31 07:32:46 -0700

    5/31

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,8 @@ export default function Home() {
       <div className="relative z-50 flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
        
       <Image
-        src="/coasysLogo_white.svg"
-        height={46}
-        width={150}
-        alt="Coasys Logo Image"
+        src="/HomePage.svg"
+        alt=""
         />
 
       </div>
