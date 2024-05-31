5c117a3560f4cd806962bb7a18f5b8e6afd2bd04
rca12377 <rca12377@gmail.com>
2024-05-30 19:09:51 -0700

    5/30

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export const dynamic = 'force-dynamic';
 export default function Home() {
   return (
 
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main id="mainDiv" className="flex min-h-screen flex-col items-center justify-between p-24">
 
 
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
