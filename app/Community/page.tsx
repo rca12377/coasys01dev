f8b48923677bf8320ae25524fb0c8849dbb33718
rca12377 <rca12377@gmail.com>
2024-05-30 19:13:27 -0700

    5/30

--- app/Community/page.tsx
+++ app/Community/page.tsx
@@ -6,15 +6,12 @@ export default function Community() {
     return (
 
         <main id="mainDiv" >
-            <h2>Community .. information</h2>
+            
+            <h1>Community .. </h1>
             <br/>
-            <Image
-              src="/ad4mLogo_white.svg"
-              height={100}
-              width={300}
-              alt="AD4M Logo Image"
-              />
+
         </main>
+        
     );
   }
   
