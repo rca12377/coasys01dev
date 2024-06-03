03dec8bdc55dc90f17227c1457e1bc046006b6b5
rca12377 <rca12377@gmail.com>
2024-06-03 11:31:07 -0700

    6/3/24

--- app/Products/page.tsx
+++ app/Products/page.tsx
@@ -6,6 +6,13 @@ export default function Products() {
     return (
 
         <main id="mainDiv" >
+
+    <div class="flex bg-green-200 p-4 mx-16 "> 
+        <div class="flex-1 bg-green-500 rounded-lg">1</div> 
+        <div class="flex-1 bg-green-500 rounded-lg">2</div> 
+        <div class="flex-1 bg-green-500 rounded-lg">3</div> 
+    </div>
+
             
             <h1>Products .. </h1>
             <br/>
