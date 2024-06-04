86396a0a734288512b7fc095fa934050d165e716
rca12377 <rca12377@gmail.com>
2024-06-04 12:03:39 -0700

    6/4/24

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,200..800&display=swap" rel="stylesheet">
+      
+        </head>
       <body className={inter.className}>
 
         <div id="PrimaryContainer">
