814278eff6673900d0da6fc221b4f82b827938c3
rca12377 <rca12377@gmail.com>
2024-06-04 12:14:37 -0700

    6/4/24

--- app/Products/page.tsx
+++ app/Products/page.tsx
@@ -8,7 +8,11 @@ export default function Products() {
         <main id="mainDiv" >
 
     <div className="flex bg-green-200 p-4 mx-16 "> 
-        <div className="flex-1 bg-green-500 rounded-lg">1</div> 
+        <div className="flex-1 bg-green-500 rounded-lg">
+
+                Primary Text <span className="SecondaryText">Secondary Text</span>! More Text here.
+        
+        </div> 
         <div className="flex-1 bg-green-500 rounded-lg">2</div> 
         <div className="flex-1 bg-green-500 rounded-lg">3</div> 
     </div>
