4bbf4e781b6292146ad6a58f7d43bfc7c9097399
rca12377 <rca12377@gmail.com>
2024-06-04 19:05:43 -0700

    6/4/24

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,17 +33,6 @@ export default function RootLayout({
 
         <div id="PrimaryContainer">
         
-          <div id="NavigationMenu">
-            <a href="/Products"><img height="40" src="/navProducts.png" alt="Products" /></a>
-            <a href="/Resources"><img height="40" src="/navResources.png" alt="Resources" /></a>
-            <a href="/Development"><img height="40" src="/navDevelopment.png" alt="Development" /></a>
-            <a href="/Community"><img height="40" src="/navCommunity.png" alt="Community" /></a>
-            &nbsp;&nbsp;
-            <a href="/"><img src="/hamburger.svg" alt="" /></a>
-            &nbsp;&nbsp;
-            <a href="/AD4M"><img src="/navLogin.svg" alt="Login" /></a>
-          </div>
-
           <div id="PageContent">
             {children}
           </div>
