559ef58b46ff9ebf66e80160209f44832681d5f0
rca12377 <rca12377@gmail.com>
2024-06-04 19:32:05 -0700

    6/4/24

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,16 @@ export default function Home() {
 
     <main id="divHomePage" className="flex min-h-screen flex-col items-center justify-between p-24">
 
-        Home Page
-
+      <div className="flex w-1/2 p-50">
+          <div className="flex-1 rounded-lg bg-gray-500/50">
+      
+              A Digital<br/>
+              Nervous Systems for<br/>
+              the <span className="SecondaryFont">New Web</span>
+            
+          </div>
+      </div>
+            
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
