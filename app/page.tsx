a8cd2ff2da04552f222ea6828d57800aa334a6de
rca12377 <rca12377@gmail.com>
2024-06-04 20:26:57 -0700

    6/4/24

--- app/page.tsx
+++ app/page.tsx
@@ -8,43 +8,42 @@ export default function Home() {
     <main id="divHomePage" className="flex min-h-screen flex-col items-center justify-between p-24">
 
       <div className="flex w-2/3 rounded-lg bg-blue-800/50 p-10 text-5xl">
-              <Image src="/coasys.svg" alt="Coasys: a Digital Nervous Systems for the New Web" />
-              <br/>
-              A Digital<br/>
-              Nervous Systems for<br/>
-              the <span className="SecondaryFont">New Web</span>
-          </div>
+          <Image src="/coasys.svg" alt="Coasys: a Digital Nervous Systems for the New Web" />
+          <br/>
+          A Digital<br/>
+          Nervous Systems for<br/>
+          the <span className="SecondaryFont">New Web</span>
       </div>
 
       <div className="flex w-2/3 p-10">
         - - What is Synergy<br/><br/>
       </div>
       
-        <div className="flex w-2/3 rounded-lg bg-blue-800/20 p-10 text-4xl">
-          A new information systems the <span className="SecondaryFont">Synergy Engine</span>
-        </div>
+      <div className="flex w-2/3 rounded-lg bg-blue-800/20 p-10 text-4xl">
+        A new information systems the <span className="SecondaryFont">Synergy Engine</span>
+      </div>
+      
+      <br/><br/>
+  
+      <div className="flex w-2/3 p-10">
+        As we transition from the information age to the age of intelligence, data with
+        provenance will become increasingly valuable.<br/>
+        <br/>We are developing a search (and reward) engine, the Synergy Engine, where data
+        with provenance becomes an asset, spawning a new information ecosystem. Use your digital
+        actions as reputation, contribute to query capacity and be rewarded.<br/>
+      </div>
         
-        <br/><br/>
+      <br/><br/>
+
+      <div className="flex-1 rounded-sm bg-green-400/80 p-10 text-lg">About the sale</div>
+        
     
-        <div className="flex w-2/3 p-10">
-          As we transition from the information age to the age of intelligence, data with
-          provenance will become increasingly valuable.<br/>
-          <br/>We are developing a search (and reward) engine, the Synergy Engine, where data
-          with provenance becomes an asset, spawning a new information ecosystem. Use your digital
-          actions as reputation, contribute to query capacity and be rewarded.<br/>
-        </div>
-          
-        <br/><br/>
 
-        <div className="flex-1 rounded-sm bg-green-400/80 p-10 text-lg">About the sale</div>
-          
-      
-
-      
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-      
+    
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+    
     </main>
   );
 }
