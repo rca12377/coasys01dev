fba92350d995faf43a62e30300d7fc9cae09f341
rca12377 <rca12377@gmail.com>
2024-06-05 07:49:14 -0700

    6/5/24

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
 
     <main id="divHomePage" className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <div className="w-2/3 item-end inline-flex">
+      <div className="w-2/3 content-end inline-flex">
 
-        <Image src="/hamburger.svg" alt="menu" />
+        <Image src="/hamburger.svg" alt="menu" className="p-10" />
 
-        <Image src="/navLogin.svg" alt="Coasys Login" />
+        <Image src="/navLogin.svg" alt="Coasys Login" className="p-10" />
         
       </div>
       
