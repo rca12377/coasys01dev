a7c1f38d0e269b26f0726411620853102edde64b
rca12377 <rca12377@gmail.com>
2024-06-05 09:19:46 -0700

    6/5/24

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
       <div className="min-w-2/3 float-right inline-flex p-10">
         <Image src="/coasys.svg" alt="Coasys: a Digital Nervous Systems for the New Web" />
-        <Image src="/spacer.gif" alt="" className="w-52 h-0" height="0" />
+        <Image src="/spacer.gif" alt="" className="w-96 h-0" height="0" />
         <Image src="/hamburger.svg" alt="menu" />
         <Image src="/spacer.gif" alt="" className="w-10 h-0" height="0" />
         <Image src="/navLogin.svg" alt="Coasys Login" />
@@ -37,17 +37,13 @@ export default function Home() {
         the <span className="SecondaryFont">Synergy Engine</span>
       </div>
       
-      <div className="h-32"><Image src="/spacer.gif" alt="" height="128" width="1" /></div>
-  
-      <div className="flex w-1/2 p-10 text-lg">
+      <div className="flex w-1/2 p-20 text-lg">
         As we transition from the information age to the age of intelligence, data with
         provenance will become increasingly valuable.<br/>
         <br/>We are developing a search (and reward) engine, the Synergy Engine, where data
         with provenance becomes an asset, spawning a new information ecosystem. Use your digital
         actions as reputation, contribute to query capacity and be rewarded.<br/>
       </div>
-        
-      <br/><br/>
 
       <div className="flex-1 rounded-lg bg-green-400/80 p-10 text-lg w-52 text-center">About the sale</div>
         
