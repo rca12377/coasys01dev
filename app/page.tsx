9b34f418ba67cb45533b9ba47a4e9d155881cb75
rca12377 <rca12377@gmail.com>
2024-06-04 20:24:43 -0700

    6/4/24

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,12 @@ export default function Home() {
 
     <main id="divHomePage" className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <div className="flex w-1/2 p-5">
-        
-          <div className="flex-1 rounded-lg bg-blue-800/50 p-10 text-4xl">
+      <div className="flex w-2/3 rounded-lg bg-blue-800/50 p-10 text-5xl">
               <Image src="/coasys.svg" alt="Coasys: a Digital Nervous Systems for the New Web" />
               <br/>
               A Digital<br/>
               Nervous Systems for<br/>
               the <span className="SecondaryFont">New Web</span>
-            
           </div>
       </div>
 
@@ -28,6 +25,7 @@ export default function Home() {
         </div>
         
         <br/><br/>
+    
         <div className="flex w-2/3 p-10">
           As we transition from the information age to the age of intelligence, data with
           provenance will become increasingly valuable.<br/>
