8c89fa7faeb0aabc121416bce973aa43123cb11a
rca12377 <rca12377@gmail.com>
2024-06-04 19:40:35 -0700

    6/4/24

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
 
     <main id="divHomePage" className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <div className="flex w-1/2 p-50">
+      <div className="flex w-1/2 p-5">
+        
           <div className="flex-1 rounded-lg bg-blue-800/50 p-10 text-4xl">
-
               <Image src="/coasys.svg" alt="Coasys: a Digital Nervous Systems for the New Web" />
               <br/>
               A Digital<br/>
@@ -18,7 +18,17 @@ export default function Home() {
             
           </div>
       </div>
+
+      <div className="flex w-2/3 p-10">
+          - - What is Synergy
+          <div className="flex-1 rounded-lg bg-blue-800/20 p-10 text-4xl">
+              
+              A new information systems the <span className="SecondaryFont">Synergy Engine</span>
             
+            </div>
+      </div>
+
+      
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
